32221481a1036ff48ea900feca4b4e9d0a7bcb81
zhangming <zhangm01ng@foxmail.com>
2020-11-20 22:03:11 +0800

    Ming:增加offercards接口

---
+++ src/main/java/com/captainxero/project/service/impl/OfferInfosListServiceImpl.java
@@ -0,0 +1,20 @@
+package com.captainxero.project.service.impl;
+
+import com.captainxero.project.entity.OfferInfosList;
+import com.captainxero.project.dao.OfferInfosListMapper;
+import com.captainxero.project.service.OfferInfosListService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Dylan
+ * @since 2020-11-20
+ */
+@Service
+public class OfferInfosListServiceImpl extends ServiceImpl<OfferInfosListMapper, OfferInfosList> implements OfferInfosListService {
+
+}

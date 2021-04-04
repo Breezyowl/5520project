14eb2aab5688b9b7b2073b551f667ab749bf22f2
zhangming <zhangm01ng@foxmail.com>
2021-04-04 22:29:26 +0800

    init the project-dylan

--- src/main/java/com/captainxero/MybatisplusApplication.java
+++ src/main/java/com/captainxero/MybatisplusApplication.java
@@ -3,15 +3,14 @@ package com.captainxero;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
-@MapperScan("com.captainxero.project.dao")
 @EnableSwagger2
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MybatisplusApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MybatisplusApplication.class, args);
     }
--- src/main/java/com/captainxero/project/controller/UserController.java
+++ src/main/java/com/captainxero/project/controller/UserController.java
@@ -1,11 +1,6 @@
 package com.captainxero.project.controller;
 
 
-import com.captainxero.project.entity.User;
-import com.captainxero.project.service.UserService;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.Map;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -34,9 +27,6 @@ import io.swagger.annotations.ApiOperation;
 public class UserController {
 
     @Autowired
-    private UserService userService;
-    
-    private static final Logger logger = LogManager.getLogger(UserController.class);
     
 	public static final String SUCCESS = "SUCCESS";
 	public static final String EXCEPTION = "EXCEPTION:";
@@ -48,18 +38,16 @@ public class UserController {
     })
     @RequestMapping(value="/login", method=RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<String> checkLogin(HttpServletResponse response,@RequestParam("userid") String userid, @RequestParam("password") String password){   	
-    	
-    	User loginUser = userService.selectUserById(userid);
-        String userPassword = loginUser.getPassword();
+    public ResponseEntity<String> checkLogin(HttpServletResponse response,@RequestParam("userid") String userid, @RequestParam("password") String password){
+        return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 
-        if(userPassword.equals(password)){
-        	Cookie user = new Cookie("userid", userid);
-        	response.addCookie(user);
-            return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
-        }else {
-            return new ResponseEntity<String>(EXCEPTION + "用户名密码错误", HttpStatus.OK);
-        }
+//        if(userPassword.equals(password)){
+//        	Cookie user = new Cookie("userid", userid);
+//        	response.addCookie(user);
+//            return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+//        }else {
+//            return new ResponseEntity<String>(EXCEPTION + "用户名密码错误", HttpStatus.OK);
+//        }
     }
     
     
